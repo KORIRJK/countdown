115fe0704b02e33bf86982d22e51619be181a03f
KORIR JAPHETH KIBET <korirjaphethkibet@gmail.com>
2025-03-31 21:19:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const minutescountdownDisplay = document.getElementById("minutescountdown-displa
 const secondscountdownDisplay = document.getElementById("secondscountdown-display")
 function renderCountdown(){
     const christmas = 25
-    // Get today's date (only the day).
+    //Get today's date (only the day).
     const countdown = new Date();
 const date1 = countdown.getDate();
 
